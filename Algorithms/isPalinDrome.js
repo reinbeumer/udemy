3dcd4cb7ee67f5897cbcc189bd03c1ebe501385e
brein <reinbeumer@gmail.com>
2018-11-10 03:07:54 +0100

    palindrome

---
+++ Algorithms/isPalinDrome.js
@@ -0,0 +1,18 @@
+function isPalindrome(string) {
+	var string = string.toLowerCase();
+	var stringArr = string.split('');
+	var valChar = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	var validStringArr = [];
+	stringArr.forEach(char => {
+		if (valChar.indexOf(char) > -1) {
+			validStringArr.push(char);
+		}
+	})
+	if (validStringArr.join() === validStringArr.reverse().join()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(isPalindrome('parteretrap'));
