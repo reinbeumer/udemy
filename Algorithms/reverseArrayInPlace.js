2c41b317b6bdc870f8eb072431a7ad23a14e4a41
brein <reinbeumer@gmail.com>
2018-11-12 22:35:10 +0100

    function reverseArrayInPlace(arr) {

---
+++ Algorithms/reverseArrayInPlace.js
@@ -0,0 +1,12 @@
+function reverseArrayInPlace(arr) {
+	var j = arr.length - 1;
+	for (var i = 0; i <= j; i++) {
+		var tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+		j--;
+	}
+	return arr;
+}
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
